9b85fba8cd653334ec946f87a9d2def8566e451a
Vladimiros Makris <vladimiros.makris@outlook.com>
2024-04-26 14:46:38 +0200

    canva_ai_video
    Vladimiros Makris

---
+++ AIVideo.java
@@ -0,0 +1,5 @@
+public class AIVideo {
+    public static void main(String[] args) {
+        System.out.println("A video created for the Tik Tok using AI (Artificial Intelligence) and the Canva app.");
+    }
+}
